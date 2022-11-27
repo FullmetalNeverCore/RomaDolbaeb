0441d42a4b326b08081405a7220deeabc60f2a02
0xNCORE <60969891+FullmetalNeverCore@users.noreply.github.com>
2022-11-27 03:00:41 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,75 @@
+import os,sys,subprocess
+import asyncio
+import urllib
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+libs = []
+
+try:
+    from apafy import pafy
+except Exception as e:
+    print(e)
+    libs.append('youtube_dl')
+    libs.append('apafy==0.5.6.1')
+
+try:
+    import discord
+    from discord import FFmpegPCMAudio, PCMVolumeTransformer
+    from discord.ext import commands, tasks
+except Exception as e:
+    print(e)
+    libs.append('discord.py')
+
+
+if (len(libs) > 0):
+    libs.append('ffmpeg')
+    for x in libs:
+        install(x)
+    sys.exit('Restart')
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = commands.Bot(command_prefix='/', intents=intents)
+FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5','options': '-vn'}
+
+@client.command()
+async def leaveRoma(ctx):
+    voicetrue = ctx.author.voice
+    mevoicetrue = ctx.guild.me.voice
+    if voicetrue is None:
+        return await ctx.send('You are not currently in the same voice channel as I am.')
+    if mevoicetrue is None:
+        return await ctx.send('Im not currently in any voice channel!')
+    await ctx.voice_client.disconnect()
+
+
+
+@client.command()
+async def silikonovieRoma(ctx):
+        if ctx.message.author.voice == None:
+            await ctx.send("You need to be in a voice channel to use this command!")
+            return
+        channel = ctx.message.author.voice.channel
+        print('Connecting to channel')
+        voice = discord.utils.get(ctx.guild.voice_channels, name=channel.name)
+        print('Getting voice channel')
+        voice_client = discord.utils.get(client.voice_clients, guild=ctx.guild)
+        if voice_client == None:
+            voice_client = await voice.connect()
+        else:
+            await voice_client.move_to(channel)
+        print('Connected!')
+        print('Getting song from yt')
+        song = pafy.new("https://www.youtube.com/watch?v=9tYgaFMwodM")  
+        audio = song.getbestaudio() 
+        print('Song parsed')
+        source = FFmpegPCMAudio(audio.url, **FFMPEG_OPTIONS)  
+        print('play song')
+        voice_client.play(source) 
+
+
+
+client.run(str(input("INPUT_TOKEN: ")))
